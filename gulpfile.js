2184be751c993fb092cfb142001e7c07cd6dde7c
jih <jih@pde.com>
2015-03-24 18:05:11 +0800

    add angular-route

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,12 @@ var uglify = require('gulp-uglify');
 var zip = require('gulp-zip');
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
-var mainBowerFiles = require('main-bower-files');
 var packageJson = require('./package.json');
 
 var paths = {
     frontLib: 'public/lib',
     scripts: 'public/lib/**/*.js'
 };
-
-gulp.task('bower', function () {
-    del.sync(paths.frontLib);
-    gulp.src(mainBowerFiles(), {base: 'bower_components'})
-        .pipe(gulp.dest(paths.frontLib));
-});
 
 gulp.task('scripts', function () {
     gulp.src(paths.scripts)
